81e0e56a10bc95caabd33279def2d34182b0f26b
Simon Chisholm <simon.chisholm@linn.co.uk>
2011-10-26 17:38:55 +0100

    Update App provider in line with recent ohNet changes

--- src/AppManager/ProviderApp.cs
+++ src/AppManager/ProviderApp.cs
@@ -8,6 +8,9 @@ namespace OpenHome.Os.AppManager
         public ProviderApp(DvDevice aDevice, IApp aApp)
             : base(aDevice)
         {
+            EnablePropertyName();
+            EnablePropertyIconUri();
+            EnablePropertyDescriptionUri();
             EnableActionGetName();
             EnableActionGetIconUri();
             EnableActionGetDescriptionUri();
@@ -15,15 +18,15 @@ namespace OpenHome.Os.AppManager
             SetPropertyIconUri(aApp.IconUri);
             SetPropertyDescriptionUri(aApp.DescriptionUri);
         }
-        protected override void GetName(uint aVersion, out string aName)
+        protected override void GetName(IDvInvocation aInvocation, out string aName)
         {
             aName = PropertyName();
         }
-        protected override void GetIconUri(uint aVersion, out string aIconUri)
+        protected override void GetIconUri(IDvInvocation aInvocation, out string aIconUri)
         {
             aIconUri = PropertyIconUri();
         }
-        protected override void GetDescriptionUri(uint aVersion, out string aDescriptionUri)
+        protected override void GetDescriptionUri(IDvInvocation aInvocation, out string aDescriptionUri)
         {
             aDescriptionUri = PropertyDescriptionUri();
         }
