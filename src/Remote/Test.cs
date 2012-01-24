e19d7d210e9ece7406070dc7f294701fd1acdc6a
Simon Chisholm <simon.chisholm@linn.co.uk>
2012-01-24 13:25:32 +0000

    Archive (very incomplete) websocket proxy.  Will shortly be reverted but the code may be handy if we revisit remote websockets later

--- src/Remote/Test.cs
+++ src/Remote/Test.cs
@@ -2,6 +2,9 @@ using System;
 using System.Net;
 using System.Threading;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
 
 namespace OpenHome.Os.Remote
 {
@@ -18,8 +21,8 @@ namespace OpenHome.Os.Remote
         private static void ProcessRequest(HttpListenerContext aContext)
         {
             const string kAppName = "/ohWidget";
-            string forwardAddress = "http://127.0.0.1:57756";
-            string resourcePath = "/7b6912d6-8ee8-4702-86ad-980574a5d6ea/Upnp/resource/";
+            string forwardAddress = "http://127.0.0.1:50610"; // TODO: should be specified by client
+            string resourcePath = "/6ad23af3-6ccd-4460-982d-34bb93f62a09/Upnp/resource/"; // TODO: should be specified by client
             HttpListenerRequest clientReq = aContext.Request;
             HttpListenerResponse clientResp = aContext.Response;
             string pathAndQuery = clientReq.Url.PathAndQuery;
@@ -30,6 +33,18 @@ namespace OpenHome.Os.Remote
             switch (method)
             {
                 case "GET":
+                    if (pathAndQuery == "/")
+                    {
+                        foreach (string key in clientReq.Headers.AllKeys)
+                        {
+                            if (key.ToUpper() == "UPGRADE" && clientReq.Headers.GetValues(key)[0].ToUpper() == "WEBSOCKET")
+                            {
+                                Console.WriteLine("!!! Ws connect attempt !!!");
+                                HandleWebSocketConnection(aContext);
+                                return;
+                            }
+                        }
+                    }
                     if (pathAndQuery.StartsWith(kAppName))
                         pathAndQuery = pathAndQuery.Remove(0, kAppName.Length);
                     if (pathAndQuery.StartsWith("/"))
@@ -41,6 +56,8 @@ namespace OpenHome.Os.Remote
                     break;
                 default:
                     Console.WriteLine("Unexpected method - {0}", method);
+                    clientResp.StatusCode = 404;
+                    clientResp.Close();
                     return;
             }
             Console.WriteLine("Method: {0}, url: {1}", clientReq.HttpMethod, targetUrl);
@@ -60,6 +77,7 @@ namespace OpenHome.Os.Remote
                         forwardedReq.TransferEncoding = clientReq.Headers.GetValues(key)[0];
                         break;
                     case "CACHE-CONTROL":
+                        // TODO
                         //forwardedReq.CachePolicy = clientReq.Headers.GetValues(key)[0];
                         break;
                     case "CONTENT-TYPE":
@@ -72,7 +90,8 @@ namespace OpenHome.Os.Remote
                         else if (String.Compare(value, "close", true) == 0)
                             connectionClose = true;
                         else
-                            Console.WriteLine("Unhandled CONNECTION header in request: {0}", clientReq.Headers.GetValues(key)[0]);
+                            forwardedReq.Connection = clientReq.Headers.GetValues(key)[0];
+                            //Console.WriteLine("Unhandled CONNECTION header in request: {0}", clientReq.Headers.GetValues(key)[0]);
                         forwardedReq.KeepAlive = connectionKeepAlive;
                         break;
                     case "ACCEPT":
@@ -83,6 +102,10 @@ namespace OpenHome.Os.Remote
                     case "ACCEPT-LANGUAGE":
                     case "ORIGIN":
                     case "SOAPACTION":
+                    //case "SEC-WEBSOCKET-PROTOCOL":
+                    //case "SEC-WEBSOCKET-KEY":
+                    //case "SEC-WEBSOCKET-VERSION":
+                    //case "UPGRADE":
                         string[] values = clientReq.Headers.GetValues(key);
                         foreach (string val in values)
                             forwardedReq.Headers.Add(key, val);
@@ -111,12 +134,14 @@ namespace OpenHome.Os.Remote
             }
             clientResp.StatusCode = (int)resp.StatusCode;
             clientResp.StatusDescription = resp.StatusDescription;
+            int contentLength = 0;
             foreach (var key in resp.Headers.AllKeys)
             {
                 switch (key.ToUpper())
                 {
                     case "CONTENT-LENGTH":
-                        clientResp.ContentLength64 = Convert.ToInt64(resp.Headers.GetValues(key)[0]);
+                        // don't set clientResp.ContentLength64 yet as we may re-write some content below (if we're serving Node.js)
+                        contentLength = Convert.ToInt32(resp.Headers.GetValues(key)[0]);
                         break;
                     case "CONTENT-TYPE":
                     case "EXT":
@@ -136,11 +161,110 @@ namespace OpenHome.Os.Remote
                         break;
                 }
             }
-            System.IO.Stream clientRespStream = clientResp.OutputStream;
-            resp.GetResponseStream().CopyTo(clientRespStream);
+            Stream clientRespStream = clientResp.OutputStream;
+            Stream respStream = resp.GetResponseStream();
+            if (!targetUrl.EndsWith("/Node.js"))
+            {
+                if (contentLength > 0) // response may be chunked
+                    clientResp.ContentLength64 = contentLength;
+                respStream.CopyTo(clientRespStream);
+            }
+            else
+            {
+                contentLength = 0; // we're re-writing content so ignore the received length
+                MemoryStream memStream = new MemoryStream();
+                MemoryStream outStream = new MemoryStream();
+                respStream.CopyTo(memStream);
+                byte[] bytes = memStream.ToArray();
+                memStream = new MemoryStream(bytes);
+                StreamReader reader = new StreamReader(memStream);
+                string line;
+                const string wsPortDecl = "var webSocketPort =";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith(wsPortDecl))
+                        line = String.Format("{0} {1};", wsPortDecl, 8080); // TODO: remove hard-coding of port
+                    line += "\r\n";
+                    contentLength += line.Length;
+                    byte[] buf = Encoding.UTF8.GetBytes(line);
+                    outStream.Write(buf, 0, buf.Length);
+                }
+                clientResp.ContentLength64 = contentLength;
+                outStream.Seek(0, SeekOrigin.Begin);
+                outStream.CopyTo(clientRespStream);
+            }
             clientRespStream.Close();
+            // docs suggest following is unnecessary - we only have to close one from clientRespStream / clientResp
             if (connectionClose)
                 clientResp.Close();
+        }
+        private static void HandleWebSocketConnection(HttpListenerContext aContext)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4);
+            socket.Connect("http://127.0.0.1", 8080); // TODO: remove hard-coding of address / port
+            const string newline = "\r\n";
+            string request = "GET / HTTP/1.1" + newline;
+            System.Collections.Specialized.NameValueCollection headers = aContext.Request.Headers;
+            foreach (string key in headers.AllKeys)
+            {
+                string header = String.Format("{0}: {1}{2}", key, headers.GetValues(key)[0], newline);
+                request += header;
+            }
+            request += newline;
+            socket.Send(Encoding.UTF8.GetBytes(request));
+            byte[] serverMsg = new byte[4*1024];
+            socket.BeginReceive(serverMsg, 0, serverMsg.Length, SocketFlags.None, WebSocketServerEvent, aContext);
+
+
+
+
+            Semaphore blocker = new Semaphore(0, 1);
+            blocker.WaitOne(); // TODO: allow exit when either side closes their socket
+
+            socket.Shutdown(SocketShutdown.Receive);
+            socket.Close();
+            aContext.Response.Close();
+        }
+        private static void WebSocketServerEvent(IAsyncResult aAr)
+        {
+        }
+    }
+
+    class WebSocketConnection
+    {
+        private readonly HttpListenerContext iContext;
+        private readonly Socket iSocket;
+
+        public WebSocketConnection(HttpListenerContext aContext)
+        {
+            iContext = aContext;
+            iSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4);
+        }
+        public void Start()
+        {
+            iSocket.Connect("http://127.0.0.1", 8080); // TODO: remove hard-coding of address / port
+            const string newline = "\r\n";
+            string request = "GET / HTTP/1.1" + newline;
+            System.Collections.Specialized.NameValueCollection headers = iContext.Request.Headers;
+            foreach (string key in headers.AllKeys)
+            {
+                string header = String.Format("{0}: {1}{2}", key, headers.GetValues(key)[0], newline);
+                request += header;
+            }
+            request += newline;
+            iSocket.Send(Encoding.UTF8.GetBytes(request));
+            byte[] serverMsg = new byte[4 * 1024];
+            iSocket.BeginReceive(serverMsg, 0, serverMsg.Length, SocketFlags.None, WebSocketServerEvent, this);
+        }
+        private static void WebSocketServerEvent(IAsyncResult aAr)
+        {
+            WebSocketConnection self = (WebSocketConnection)aAr.AsyncState;
+            SocketError error;
+            self.iSocket.EndReceive(aAr, out error);
+            if (error != SocketError.Success)
+            {
+                // TODO: do something to close connections
+            }
         }
     }
 
