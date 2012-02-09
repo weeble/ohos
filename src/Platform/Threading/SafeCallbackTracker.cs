9be73feafbd7fee2216904ca414885caa9bd73f8
andreww <andrew.wilson@linn.co.uk>
2012-02-09 10:04:30 +0000

    Refactor common code in SafeCallbackTracker, also provide simpler usage.

--- src/Platform/Threading/SafeCallbackTracker.cs
+++ src/Platform/Threading/SafeCallbackTracker.cs
@@ -7,74 +7,49 @@ namespace OpenHome.Widget.Nodes.Threading
         private object iMonitor = new object();
         private bool iTerminating = false;
         private int iInflight = 0;
+
+        /// <summary>
+        /// Perform aAction, unless Close() has been called. Guarantee that
+        /// aAction will not begin after Close() has returned, and will end
+        /// before Close() returns.
+        /// </summary>
+        /// <param name="aAction"></param>
+        public void PreventClose(Action aAction)
+        {
+           lock (iMonitor)
+           {
+               if (iTerminating) { return; }
+               iInflight += 1;
+           }
+           try
+           {
+               aAction();
+           }
+           finally
+           {
+               lock (iMonitor)
+               {
+                   iInflight -= 1;
+                   System.Threading.Monitor.Pulse(iMonitor);
+               }
+           }
+        }
+
+        public EventHandler<T> Create<T>(EventHandler<T> aAction) where T : EventArgs
+        {
+            return (aSender, aArgs) => PreventClose(() => aAction(aSender, aArgs));
+        }
         public Action Create(Action aAction)
         {
-            return () =>
-                       {
-                           lock (iMonitor)
-                           {
-                               if (iTerminating) { return; }
-                               iInflight += 1;
-                           }
-                           try
-                           {
-                               aAction();
-                           }
-                           finally
-                           {
-                               lock (iMonitor)
-                               {
-                                   iInflight -= 1;
-                                   System.Threading.Monitor.Pulse(iMonitor);
-                               }
-                           }
-                       };
+            return () => PreventClose(aAction);
         }
         public Action<T1,T2> Create<T1,T2>(Action<T1,T2> aAction)
         {
-            return (aArg1, aArg2) =>
-                       {
-                           lock (iMonitor)
-                           {
-                               if (iTerminating) { return; }
-                               iInflight += 1;
-                           }
-                           try
-                           {
-                               aAction(aArg1, aArg2);
-                           }
-                           finally
-                           {
-                               lock (iMonitor)
-                               {
-                                   iInflight -= 1;
-                                   System.Threading.Monitor.Pulse(iMonitor);
-                               }
-                           }
-                       };
+            return (aArg1, aArg2) => PreventClose(() => aAction(aArg1, aArg2));
         }
         public Action<T1> Create<T1>(Action<T1> aAction)
         {
-            return (aArg) =>
-                       {
-                           lock (iMonitor)
-                           {
-                               if (iTerminating) { return; }
-                               iInflight += 1;
-                           }
-                           try
-                           {
-                               aAction(aArg);
-                           }
-                           finally
-                           {
-                               lock (iMonitor)
-                               {
-                                   iInflight -= 1;
-                                   System.Threading.Monitor.Pulse(iMonitor);
-                               }
-                           }
-                       };
+            return aArg => PreventClose(() => aAction(aArg));
         }
         /// <summary>
         /// Prevent all further callbacks. Block until no callbacks
