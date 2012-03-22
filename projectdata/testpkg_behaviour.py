be34addd89c818b8cf428f0d9db5e0cea6601eea
Andrew Wilson <andrew.wilson@linn.co.uk>
2012-03-22 11:00:54 +0000

    Fix error during clean if nothing to clean.

--- projectdata/testpkg_behaviour.py
+++ projectdata/testpkg_behaviour.py
@@ -56,7 +56,7 @@ def process_optional_steps(context):
 @build_step("clean", optional=True, default=False)
 def pkgtest(context):
     for pkgdir in context.options.clean:
-        shell('rm "{0}/*.deb" "{0}/*.changes" "{0}/*.tar.gz" "{0}/*.dsc"'.format(pkgdir))
+        shell('rm -f "{0}/*.deb" "{0}/*.changes" "{0}/*.tar.gz" "{0}/*.dsc"'.format(pkgdir))
 
 def globuniq(pattern):
     result = glob(pattern)
