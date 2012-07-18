abb21cdd6c584f314bdc56db23bcb32cd227241c
Chris Cheung <chris.cheung@linn.co.uk>
2012-07-18 12:54:00 +0100

    First draft of borderlayout control

---
+++ src/Xohj/ohj/borderlayout/ohj.borderlayout.js
@@ -0,0 +1,60 @@
+/*
+* BorderLayout
+*/ 
+
+;(function($) {
+    ohjui['ohjborderlayout'] = function(element, options) {
+            var elem = $(element);
+            var _this = this;
+            var settings = $.extend({
+                extend: null,
+                topheight: null,
+                bottomheight: null,
+                leftwidth: null,
+                rightwidth: null
+            }, options || {});
+            
+            
+
+            // Public Methods / Virtual Methods
+            this.doSomething = function() {
+                
+            };
+            
+            // Private Methods
+            var render = function() {
+                elem.initPlugin('ohjborderlayout');
+                elem.hookPlugin(settings);
+                
+                var mainLayout = elem.children('.ohjborderlayout-main');
+                var leftLayout = mainLayout.children('.ohjborderlayout-left');
+                var midLayout = mainLayout.children('.ohjborderlayout-middle');
+                var rightLayout = mainLayout.children('.ohjborderlayout-right');
+                var topLayout = elem.children('.ohjborderlayout-top');
+                var bottomLayout = elem.children('.ohjborderlayout-bottom');
+                
+
+                if(settings.topheight == null) settings.topheight = $.fn.getOuterHeight(topLayout);
+                if(settings.bottomheight == null) settings.bottomheight = $.fn.getOuterHeight(bottomLayout);
+                if(settings.leftwidth == null) settings.leftwidth = $.fn.getOuterWidth(leftLayout);
+                if(settings.rightwidth == null) settings.rightwidth = $.fn.getOuterWidth(rightLayout);
+
+                topLayout.css({ 'height': settings.topheight });
+                leftLayout.css({ 'width': settings.leftwidth });
+                bottomLayout.css({ 'height': settings.bottomheight });
+                mainLayout.css({ 'top' : settings.topheight,
+                                  'bottom' : settings.bottomheight });
+                rightLayout.css({ 'width': settings.rightwidth });
+                midLayout.css({ 'left' : settings.leftwidth,
+                                'right' : settings.rightwidth });
+            };
+            
+            // Add / override extensions to load
+            if(settings.extend)
+                settings.extend.call(this,elem,settings);
+            render();
+    };
+    
+    $.fn.createPlugin('ohjborderlayout');
+
+})(window.jQuery || window.Zepto);
