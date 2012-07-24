4c7a16d79a5e6e963df25b4420adc3dc28837cbc
Chris Cheung <chris.cheung@linn.co.uk>
2012-07-24 11:41:10 +0100

    Removed unnecessary code within borderlayout

--- src/Xohj/ohj/borderlayout/ohj.borderlayout.js
+++ src/Xohj/ohj/borderlayout/ohj.borderlayout.js
@@ -19,38 +19,30 @@
             // Private Methods
             var render = function() {
                 elem.hookPlugin(settings);
-
                 if(settings.height == null) settings.height = $.fn.getOuterHeight(elem);
                 elem.css({'height': settings.height });
-                if( $.fn.getOuterHeight(elem) > 0)
-                {
-                    var mainLayout = elem.children('.ohjborderlayout-main');
-                    var leftLayout = mainLayout.children('.ohjborderlayout-left');
-                    var midLayout = mainLayout.children('.ohjborderlayout-middle');
-                    var rightLayout = mainLayout.children('.ohjborderlayout-right');
-                    var topLayout = elem.children('.ohjborderlayout-top');
-                    var bottomLayout = elem.children('.ohjborderlayout-bottom');
+                var mainLayout = elem.children('.ohjborderlayout-main');
+                var leftLayout = mainLayout.children('.ohjborderlayout-left');
+                var midLayout = mainLayout.children('.ohjborderlayout-middle');
+                var rightLayout = mainLayout.children('.ohjborderlayout-right');
+                var topLayout = elem.children('.ohjborderlayout-top');
+                var bottomLayout = elem.children('.ohjborderlayout-bottom');
                 
-                    if(settings.topheight == null) settings.topheight = $.fn.getOuterHeight(topLayout);
-                    if(settings.bottomheight == null) settings.bottomheight = $.fn.getOuterHeight(bottomLayout);
-                    if(settings.leftwidth == null) settings.leftwidth = $.fn.getOuterWidth(leftLayout);
-                    if(settings.rightwidth == null) settings.rightwidth = $.fn.getOuterWidth(rightLayout);
+                if(settings.topheight == null) settings.topheight = $.fn.getOuterHeight(topLayout);
+                if(settings.bottomheight == null) settings.bottomheight = $.fn.getOuterHeight(bottomLayout);
+                if(settings.leftwidth == null) settings.leftwidth = $.fn.getOuterWidth(leftLayout);
+                if(settings.rightwidth == null) settings.rightwidth = $.fn.getOuterWidth(rightLayout);
                     
 
-                    topLayout.css({ 'height': settings.topheight || 0 });
-                    leftLayout.css({ 'width': settings.leftwidth || 0});
-                    bottomLayout.css({ 'height': settings.bottomheight || 0 });
-                    mainLayout.css({ 'top' : settings.topheight || 0,
-                                      'bottom' : settings.bottomheight || 0 });
-                    rightLayout.css({ 'width': settings.rightwidth || 0});
-                    midLayout.css({ 'left' : settings.leftwidth || 0 ,
-                                    'right' : settings.rightwidth || 0 });
-               }
-               else
-               {
-                    elem.hide();
-                    throw 'BorderLayout could not determine the height of the container.';
-               }
+                topLayout.css({ 'height': settings.topheight || 0 });
+                leftLayout.css({ 'width': settings.leftwidth || 0});
+                bottomLayout.css({ 'height': settings.bottomheight || 0 });
+                mainLayout.css({ 'top' : settings.topheight || 0,
+                                    'bottom' : settings.bottomheight || 0 });
+                rightLayout.css({ 'width': settings.rightwidth || 0});
+                midLayout.css({ 'left' : settings.leftwidth || 0 ,
+                                'right' : settings.rightwidth || 0 });
+  
             };
             
             // Add / override extensions to load
